1aa9edde709873b718c1b4ddd95f2d30f397bf2d
Diptanshu <sinha.diptanshu10@gmail.com>
2020-05-02 00:06:54 +0530

    Created the JS based app

--- cm.py
+++ cm.py
@@ -1,41 +1,24 @@
 import numpy as np
+import sys
 import pandas as pd
 import nltk
-#nltk.download('punkt') # one time execution
 import re
-#f1=open('/home/diptanshu/Desktop/google.txt',mode='r')
-#file=open('/home/diptanshu/Documents/checkmate/chetti.txt',mode='r')
-#a1=f1.read()
-#all_of_it=file.read()
-#print(all_of_it)
-#file.close()
-def abc(all_of_it):                                                                         # input is string containing all the data, needs to be read from pdf
-    import numpy as np
-    import pandas as pd
-    import nltk
-    #nltk.download('punkt') # one time execution
-    import re
-    #f1=open('/home/diptanshu/Desktop/google.txt',mode='r')
-    #file=open('/home/diptanshu/Documents/checkmate/chetti.txt',mode='r')
-    #a1=f1.read()
-    #all_of_it=file.read()
-    #print(all_of_it)
-    #file.close()
+
+def abc(path_to_file):
+    file=open(path_to_file,'r+')
+    all_of_it=file.read()
     from nltk.tokenize import sent_tokenize
     sentences=[]    
-    #sentences.append(sent_tokenize(a1))
     sentences.append(sent_tokenize(all_of_it))
     sentences = [y for x in sentences for y in x]
-    #print(sentences)
     word_embeddings = {}
-    f = open('/glove.6B.100d.txt', encoding='utf-8')
+    f = open('../glove.6B.100d.txt', encoding='utf-8')
     for line in f:
         values = line.split()
         word = values[0]
         coefs = np.asarray(values[1:], dtype='float32')
         word_embeddings[word] = coefs
     f.close()
-    #print(len(word_embeddings))
     clean_sentences = pd.Series(sentences).str.replace("[^a-zA-Z]", " ")
     clean_sentences = [s.lower() for s in clean_sentences]
     from nltk.corpus import stopwords
@@ -45,7 +28,7 @@ def abc(all_of_it):                                                             
         return sen_new
     clean_sentences = [remove_stopwords(r.split()) for r in clean_sentences]
     word_embeddings = {}
-    f = open('love.6B.100d.txt', encoding='utf-8')
+    f = open('../glove.6B.100d.txt', encoding='utf-8')
     for line in f:
         values = line.split()
         word = values[0]
@@ -53,7 +36,6 @@ def abc(all_of_it):                                                             
         word_embeddings[word] = coefs
     f.close()
     sentence_vectors = []
-    #print(len(clean_sentences))
     for i in clean_sentences:
         if len(i) != 0:
             v = sum([word_embeddings.get(w, np.zeros((100,))) for w in i.split()])/(len(i.split())+0.001)
@@ -81,3 +63,5 @@ def abc(all_of_it):                                                             
         if c==9:
             break
     return a
+
+print(abc(sys.argv[1]))
